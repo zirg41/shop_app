a052640d5b7b10d824e2b68c0927b1017962e1c0
Фаиз <galimow63@gmail.com>
2022-02-12 09:22:08 +0400

    Image.network(imageUrl)

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem({this.id, this.imageUrl, this.title});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridTile(
+      child: Image.network(imageUrl),
+    );
   }
 }
