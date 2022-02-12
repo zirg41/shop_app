bce7b7e2ea43bfd2577d5b8dd9e1b5e7d75d5769
Фаиз <galimow63@gmail.com>
2022-02-12 12:10:04 +0400

    asdcfWFWESDVDSZVFD

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -9,6 +9,8 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("title"),
