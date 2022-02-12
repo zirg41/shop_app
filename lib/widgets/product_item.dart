6a4cec637bee9839438b66516b3965ab924e2430
Фаиз <galimow63@gmail.com>
2022-02-12 09:42:14 +0400

    GridTileBar added

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -10,7 +10,19 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(imageUrl),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
