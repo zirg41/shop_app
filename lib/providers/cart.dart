723e9ad02644411c3abed39d0e8c33fd214c219f
Фаиз <galimow63@gmail.com>
2022-02-17 20:50:31 +0400

    totalAmount getter added

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -24,6 +24,16 @@ class Cart with ChangeNotifier {
     return _items.length;
   }
 
+  double get totalAmount {
+    double total = 0.0;
+
+    _items.forEach((key, cartValue) {
+      total += cartValue.price * cartValue.quantity;
+    });
+
+    return total;
+  }
+
   void addItem(String productId, double price, String title) {
     if (_items.containsKey(productId)) {
       _items.update(
