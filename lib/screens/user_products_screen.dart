d6a977e43153823518cec2e4cac62de41b9d5c5b
Фаиз <galimow63@gmail.com>
2022-02-19 19:18:44 +0400

    UserProductsScreen added

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = "/user-products-screen";
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your products"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (_, index) => Column(
+            children: [
+              UserProductItem(
+                title: productsData.items[index].title,
+                imageUrl: productsData.items[index].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+          itemCount: productsData.items.length,
+        ),
+      ),
+    );
+  }
+}
