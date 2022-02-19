d8e905db100b7a24ef80b931dee7ab280cff499f
Фаиз <galimow63@gmail.com>
2022-02-19 12:26:05 +0400

    deleting unused mainAxisAlignment

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -24,7 +24,7 @@ class CartScreen extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
                     "Total",
