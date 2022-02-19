e2d2096f8d02baaa8dcc75231efc37924c741c27
Фаиз <galimow63@gmail.com>
2022-02-19 17:52:49 +0400

    total showing result fixed

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -54,7 +54,7 @@ class CartItem extends StatelessWidget {
               ),
             ),
             title: Text(title),
-            subtitle: Text("Total: \$${price * quantity}"),
+            subtitle: Text("Total: \$${(price * quantity).toStringAsFixed(2)}"),
             trailing: Text('$quantity x'),
           ),
         ),
