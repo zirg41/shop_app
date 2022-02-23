40dd8506e9ae59fea36051c5f29cad04a1c444ea
Фаиз <galimow63@gmail.com>
2022-02-23 11:46:39 +0400

    changing childAspectRatio

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -30,7 +30,7 @@ class ProductsGrid extends StatelessWidget {
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
+        childAspectRatio: 2 / 2,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
