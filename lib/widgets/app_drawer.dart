0126022bb0524502fcc251df8b08bf0fb2a6b60c
Фаиз <galimow63@gmail.com>
2022-02-23 19:29:55 +0400

    logging out added

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/auth.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
@@ -37,6 +39,16 @@ class AppDrawer extends StatelessWidget {
             onTap: () => Navigator.of(context)
                 .pushReplacementNamed(UserProductsScreen.routeName),
           ),
+          Divider(),
+          ListTile(
+              leading: Icon(
+                Icons.exit_to_app,
+              ),
+              title: Text("Log out"),
+              onTap: () {
+                Navigator.of(context).pop();
+                Provider.of<Auth>(context, listen: false).logout();
+              }),
         ],
       ),
     );
